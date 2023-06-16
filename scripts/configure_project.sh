129b84a0ba0a170139ebbb2ca2a8230ad1e6cecf
Shahondin1624 <shahondin1624@gmail.com>
2023-06-16 18:24:24 +0200

    fixed having to delete the cloned exporter each time when rerunning the script and a cd error

--- scripts/configure_project.sh
+++ scripts/configure_project.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-git clone https://github.com/Shahondin1624/github-exporter-arm64-rs.git &&
-cd github-exporter-arm64-rs/ &&
+if [ -d "github-exporter-arm64-rs" ]; then
+    cd github-exporter-arm64-rs/
+    git pull
+else
+    git clone https://github.com/Shahondin1624/github-exporter-arm64-rs.git &&
+    cd github-exporter-arm64-rs/
+fi
 docker build -t github-exporter-arm64-rs . &&
-cd .. &&
-cd ../docker &&
+cd ../ &&
+cd IT-Tec_Prometheus/docker &&
 sudo TOKEN="${1}" docker compose up -d #enter your github api-token here
